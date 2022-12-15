b66373a27aca5feb35e43a204a99202d39b2dc56
GjLuffy <gui.henmelo@gmail.com>
2022-12-15 00:09:24 -0300

    url azure

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: `https://fiap-notes-api-auth.herokuapp.com/`,
+  baseURL: `https://note-service-back-end.azurewebsites.net/`,
 });
 
 api.interceptors.request.use(async (config) => {
